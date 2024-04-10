ab22dd86d6316280457ffad899565c87ac376c76
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-04-11 08:52:48 +1000

    update index make_index to include ingredients, equipment. Sort alphabetically

--- bin/make_index.py
+++ bin/make_index.py
@@ -2,20 +2,31 @@
 
 import os
 from pathlib import Path
+from natsort import natsorted
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
-def generate_index_markdown(folder_path: Path=PROJECT_ROOT):
+def get_files(folder_path: Path=PROJECT_ROOT):
     markdown_files = [file for file in folder_path.glob('*.md')]
+    return natsorted(markdown_files)
 
-    with open(f"{folder_path / 'index.md'}", 'w') as index_file:
-        index_file.write("# Index\n\n")
-        index_file.write("## Recipes\n\n")
-        for file in markdown_files:
 
-            if file.name in ('index.md', 'README.md'):
-                continue
-            index_file.write(f"- [[{file.stem}]]\n")
+def main():
+
+     with open(f"{PROJECT_ROOT / 'index.md'}", 'w') as index_file:
+        index_file.write(f"# Index")
+
+        for item in ["Recipes", "Ingredients", "Equipments"]:
+            folder_path = PROJECT_ROOT / item if item != "Recipes" else PROJECT_ROOT
+            markdown_files = get_files(folder_path)
+            index_file.write(f"\n\n## {item}\n\n")
+                
+            for file in markdown_files:
+                if file.stem in ["index", "README"]:
+                    continue
+                
+                index_file.write(f"- [[{file.stem}]]\n")
+
 
 if __name__ == '__main__':
-    generate_index_markdown()
+    main()
